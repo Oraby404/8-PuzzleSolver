5b212e2c2557562e674cfd36b96f67e5cbc3a0c7
Oraby404 <61716842+Oraby404@users.noreply.github.com>
2021-05-07 21:23:18 +0200

    fixed move functions and added expand function

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from copy import deepcopy
 class Puzzle:
     def __init__(self, board_config):
         self.board_config = board_config
-        # self.parent = parent
         self.children = []
+        # self.parent = parent
         # self.action = action
-        self.depth = 0
-        self.heuristic = 0
+        # self.depth = 0
+        # self.heuristic = 0
 
         for i in range(3):
             for j in range(3):
@@ -21,45 +21,70 @@ class Puzzle:
         if self.blank_row == 1:
             return None
         else:
-            self.blank_row -= 1
+            new_node = Puzzle(deepcopy(self.board_config))
+            new_node.blank_row -= 1
             i = self.blank_row - 1
             j = self.blank_col - 1
-            new_node = deepcopy(self.board_config)
-            new_node[i + 1][j], new_node[i][j] = new_node[i][j], new_node[i + 1][j]
-            return Puzzle(new_node)
+            new_node.board_config[i - 1][j], new_node.board_config[i][j] = new_node.board_config[i][j], \
+                                                                           new_node.board_config[i - 1][j]
+            return new_node
 
     def down(self):
         if self.blank_row == 3:
             return None
         else:
-            self.blank_row += 1
+            new_node = Puzzle(deepcopy(self.board_config))
+            new_node.blank_row += 1
             i = self.blank_row - 1
             j = self.blank_col - 1
-            new_node = deepcopy(self.board_config)
-            new_node[i - 1][j], new_node[i][j] = new_node[i][j], new_node[i - 1][j]
-            return Puzzle(new_node)
+            new_node.board_config[i + 1][j], new_node.board_config[i][j] = new_node.board_config[i][j], \
+                                                                           new_node.board_config[i + 1][j]
+            return new_node
 
     def left(self):
         if self.blank_col == 1:
             return None
         else:
-            self.blank_col -= 1
+            new_node = Puzzle(deepcopy(self.board_config))
+            new_node.blank_col -= 1
             i = self.blank_row - 1
             j = self.blank_col - 1
-            new_node = deepcopy(self.board_config)
-            new_node[i][j + 1], new_node[i][j] = new_node[i][j], new_node[i][j + 1]
-            return Puzzle(new_node)
+            new_node.board_config[i][j - 1], new_node.board_config[i][j] = new_node.board_config[i][j], \
+                                                                           new_node.board_config[i][j - 1]
+            return new_node
 
     def right(self):
         if self.blank_col == 3:
             return None
         else:
-            self.blank_col += 1
+            new_node = Puzzle(deepcopy(self.board_config))
+            new_node.blank_col += 1
             i = self.blank_row - 1
             j = self.blank_col - 1
-            new_node = deepcopy(self.board_config)
-            new_node[i][j - 1], new_node[i][j] = new_node[i][j], new_node[i][j - 1]
-            return Puzzle(new_node)
+            new_node.board_config[i][j + 1], new_node.board_config[i][j] = new_node.board_config[i][j], \
+                                                                           new_node.board_config[i][j + 1]
+            return new_node
+
+    def expand(self):
+        if len(self.children) == 0:
+
+            up_child = self.up()
+            if up_child is not None:
+                self.children.append(up_child)
+
+            down_child = self.down()
+            if down_child is not None:
+                self.children.append(down_child)
+
+            left_child = self.left()
+            if left_child is not None:
+                self.children.append(left_child)
+
+            right_child = self.right()
+            if right_child is not None:
+                self.children.append(right_child)
+
+        return self.children
 
 
 goal = [[0, 1, 2],
@@ -69,3 +94,20 @@ goal = [[0, 1, 2],
 board = Puzzle([[1, 2, 5],
                 [3, 0, 4],
                 [6, 7, 8]])
+
+print(board.blank_row)
+print(board.blank_col)
+
+board.children = board.expand()
+
+print(board.children[0].blank_row)
+print(board.children[0].blank_col)
+
+print(board.children[1].blank_row)
+print(board.children[1].blank_col)
+
+print(board.children[2].blank_row)
+print(board.children[2].blank_col)
+
+print(board.children[3].blank_row)
+print(board.children[3].blank_col)
